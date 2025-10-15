908926fcc3ee9f759255c79f6cd897faf0c1c288
Ruoyu50 <sssssmess@foxmail.com>
2025-10-15 18:19:40 -0400

    save progress for the selectableGallery

--- src/App.jsx
+++ src/App.jsx
@@ -32,23 +32,81 @@
 
 
 
-import { useState } from 'react';
-export default function ToggleImage() {
-  const [visible, setVisible] = useState(true);
+// import { useState } from 'react';
+// export default function ToggleImage() {
+//   const [visible, setVisible] = useState(true);
 
-  return (
-    <div>
-      <button onClick={() => setVisible(!visible)}>
-        {visible ? 'Hide' : 'Show'}
-      </button>
-      {visible && (
-        <div>
-          <p>This is a sample image:</p>
-          <img src="https://picsum.photos/id/237/200/300"
-          alt="React Logo"
-          style={{ width: '200px', marginTop: '20px' }} />
-        </div>
+//   return (
+//     <div>
+//       <button onClick={() => setVisible(!visible)}>
+//         {visible ? 'Hide' : 'Show'}
+//       </button>
+//       {visible && (
+//         <div>
+//           <p>This is a sample image:</p>
+//           <img src="https://picsum.photos/id/237/200/300"
+//           alt="React Logo"
+//           style={{ width: '200px', marginTop: '20px' }} />
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+
+
+
+import { useState } from 'react';
+
+export default function Gallery() {
+  const [images] = useState([
+    "https://picsum.photos/id/237/200/300",
+    "https://picsum.photos/id/238/200/300",
+    "https://picsum.photos/id/239/200/300",
+  ])
+
+// return(
+//     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+//       {images.map((url, index) =>(
+//         <img key={index}
+//           src={url}
+//           alt={`photo ${index}`}
+//           style={{ width: '200px', margin: '10px' }}
+//         />)
+//       )}
+//       </div>
+// )
+
+// function myFunction (url,index){
+  
+// }
+
+const toggleSelect = (url) => {
+  const images = {
+    const currentSelected = [...selected];
+
+    if (currentSelected.includes(url)){
+      const index  = currentSelect.index0f(url);
+      currentSelected.splice(index, 1)
+    } else {
+      custoSelected.push(url);
+    }
+    setSelected(currentSelected);
+  }
+
+
+  return(
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      {images.map((url, index) =>(
+        <img key={index}
+          src={url}
+          alt={`photo ${index}`}
+          onClick={() => toggleSelect(url)}
+          style={{
+            border: selected.includes(url) ? '5px solid blue' : 'none' 
+          }}
+        />)
       )}
-    </div>
-  );
-}
+      </div>
+    )
+  }
